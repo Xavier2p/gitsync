64b94891b2af2479ccf23d0316420aa032f3d220
Xavier2p <Xavier2p@users.noreply.github.com>
2023-12-19 10:18:37 +0000

    deploy: 2ccc29034f39b439e30febd86d1d13398a612bcc

--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gsync":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"gsync/args/struct.Args.html\" title=\"struct gsync::args::Args\">Args</a>",1,["gsync::args::Args"]]]
+"gsync":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"gsync/args/struct.Args.html\" title=\"struct gsync::args::Args\">Args</a>",1,["gsync::args::Args"]]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"gsync":["",[],["args.rs","git.rs","main.rs"]]\
+}');
+createSrcSidebar();
